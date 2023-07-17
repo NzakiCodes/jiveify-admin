bd3216950823a23f6206b589fd21d10ed5694150
Ekikere-abasi Ekere <ekisnzaki@gmail.com>
2023-07-17 09:35:52 +0100

    Adjusted Podcasters table footer

--- app/dashboard/podcasters/data-table-pagination.tsx
+++ app/dashboard/podcasters/data-table-pagination.tsx
@@ -23,7 +23,7 @@ import {
     table,
   }: DataTablePaginationProps<TData>) {
     return (
-      <div className="flex items-center justify-between px-4 py-4">
+      <div className="flex flex-col gap-y-3 lg:items-center justify-between px-4 py-4">
         <div className="flex-1 text-sm text-muted-foreground">
           {table.getFilteredSelectedRowModel().rows.length} of{" "}
           {table.getFilteredRowModel().rows.length} row(s) selected.

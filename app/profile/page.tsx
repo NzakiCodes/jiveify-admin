159df9a2b323ddb0420a53cfc10fa7cbef8c4751
Ekikere-abasi Ekere <ekisnzaki@gmail.com>
2023-07-17 21:44:01 +0100

    Addddd

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,17 +6,30 @@ import React from "react";
 
 function Page() {
   return (
-    <div>
+    <div className="p-16">
       <div>
-        <div>
-            <Link href={basePath}>
-                <Button variant={"outline"} className="rounded-full" size={"icon"}>
-                    <ChevronLeft/>
-                </Button>
-            </Link>
+        <Link
+          href={basePath}
+          className="flex items-center gap-x-4 w-24 hover:opacity-95"
+        >
+          <Button variant={"outline"} className="rounded-full" size={"icon"}>
+            <ChevronLeft />
+          </Button>
+          <span>Back</span>
+        </Link>
+      </div>
+      <div className="py-6 px-14">
+        <div className="flex justify-between items-center gap-x-1">
+          <div className="flex flex-col gap-y-4">
+            <h3 className="text-2xl font-heading font-bold">Settings Page</h3>
+            <p>Update your photo and personal details here.</p>
+          </div>
+          <div className="space-x-2">
+            <Button variant={"outline"} size={"lg"}>Cancel</Button>
+            <Button variant={"ghost"} size={"lg"} className="bg-purple-600 hover:bg-purple-600">Save</Button>
+          </div>
         </div>
       </div>
-      <div></div>
     </div>
   );
 }

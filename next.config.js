5bcb5ca1f0f25172138ed893ddc15b8039e52011
Ekikere-abasi Ekere <ekisnzaki@gmail.com>
2023-07-31 08:08:47 +0100

    Added new image domain to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["herokuapp.com","googleapis.com"]
+        domains:["herokuapp.com","firebasestorage.googleapis.com"]
     }
 }
 

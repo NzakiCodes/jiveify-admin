4d2a2a10033c0a48c5eaf1769ebcd54197ffdfbb
Ekikere-abasi Ekere <ekisnzaki@gmail.com>
2023-08-15 09:00:04 +0100

    Addedddd

--- app/dashboard/published-podcasts/page.tsx
+++ app/dashboard/published-podcasts/page.tsx
@@ -10,7 +10,6 @@ import { useQuery } from "@tanstack/react-query";
 import PodcastCard from "@/components/podcast-card";
 import { Podcast } from "@/types";
 
-
 const fetchPopularPodcasts = (): Promise<Podcast[]> =>
   axiosAuth.get("/podcast/popular").then((response) => response.data);
 
@@ -18,8 +17,6 @@ const fetchRecentViewedPodcasts = (): Promise<Podcast[]> =>
   axiosAuth.get("/podcast/recent").then((response) => response.data);
 
 export default function Page() {
- 
-
   const popularPodcastQuery = useQuery(
     ["popular-podcast"],
     fetchPopularPodcasts
@@ -29,8 +26,6 @@ export default function Page() {
     ["recent-viewed-podcast"],
     fetchRecentViewedPodcasts
   );
-
-
 
   useEffect(() => {}, []);
 
@@ -44,34 +39,32 @@ export default function Page() {
               <h2 className="text-2xl font-semibold tracking-tight">
                 All Published Podcasts
               </h2>
-            
             </div>
-            
           </div>
           <Separator className="my-4" />
           <div className="relative">
             {/* <ScrollArea> */}
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pb-4">
-                {popularPodcastQuery.isLoading &&
-                  [1, 2, 3, 4, 5,6,7,8,9,10,9,8,9].map((p) => (
-                    <PodcastCard.Skelecton
-                      key={p}
-                      className="w-[250px] h-[330px]"
-                    />
-                  ))}
-                {popularPodcastQuery.isSuccess &&
-                  popularPodcastQuery.data.map((podcast) => (
-                    <PodcastCard
-                      key={podcast.id}
-                      podcast={podcast}
-                      className="w-[250px]"
-                      aspectRatio="portrait"
-                      width={250}
-                      height={330}
-                    />
-                  ))}
-              </div>
-              {/* <ScrollBar orientation="horizontal" />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pb-4">
+              {popularPodcastQuery.isLoading &&
+                Array.from({ length: 12 }).map((_, idx) => (
+                  <PodcastCard.Skelecton
+                    key={idx}
+                    className="w-[250px] h-[330px]"
+                  />
+                ))}
+              {popularPodcastQuery.isSuccess &&
+                popularPodcastQuery.data.map((podcast) => (
+                  <PodcastCard
+                    key={podcast.id}
+                    podcast={podcast}
+                    className="w-[250px]"
+                    aspectRatio="portrait"
+                    width={250}
+                    height={330}
+                  />
+                ))}
+            </div>
+            {/* <ScrollBar orientation="horizontal" />
             </ScrollArea> */}
           </div>
         </div>
